45a5057266264ecb82bac0d6c52ded183fe619f2
Bruno Gianini <blgianini@gmail.com>
2025-01-24 12:02:48 -0300

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    github = {
+        source = "integrations/github"
+        version = "~-> 6.0"
+    }
+  }
+  
+}
+
+provider "github" {}
+
+resource "github_repository" "teste" {
+    name = "teste"
+    descrption = "teste"
+
+    visibility = "public"
+}
